91ffca2841c90f53e48d43f70836424bce341e16
sanjeev13 <35870801+sanjeev13@users.noreply.github.com>
2018-12-27 15:02:18 +0530

    replaced private key with placeholder

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 var config = {};
 
-config.privateKey = "0x8ee59a190870a361e482c4ec1db59498532b7f4111ea5501100a0773cceca4c5";
+config.privateKey = "your_private_key";
 config.network = "ropsten";
 
 module.exports = config;
